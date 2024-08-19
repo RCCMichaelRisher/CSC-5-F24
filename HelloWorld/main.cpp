a578dbe1a0ea67c9176167d0f186ae5a4e423da8
Michael Risher <rishermichael@gmail.com>
2024-08-18 19:03:07 -0700

    Hello world uploaded

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,36 @@
+/* 
+ * File:   main.cpp
+ * Author: Michael Risher
+ *
+ * Created on August 18, 2024, 6:33 PM
+ */
+
+// System libraries
+#include <iostream>
+
+using namespace std;
+
+// user libraries (if any)
+
+// global constants
+// used for science, math, conversion, dimensions. Things that never change
+
+// function prototypes
+
+//execution starts here at the main function
+int main(int argc, char** argv) {    
+    // declare variables
+    
+    // initialize variables
+    
+    // process the inputs -> outputs (IO)
+    
+    // display inputs/outputs
+    cout<<"Hello World"<<endl;
+    
+    // clean up memory and files
+    
+    // exit the Program
+    return 0;
+}
+
