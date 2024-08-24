06d48788eb39e19451188c4fac2bf92db782dba7
Michael Risher <michael.risher@rccd.edu>
2024-08-23 18:03:19 -0700

    added template

---
+++ Template/main.cpp
@@ -0,0 +1,34 @@
+/* 
+ * File:   main.cpp
+ * Author: <YOUR NAME HERE>
+ * Created: <WHEN DID YOU MAKE THIS?>
+ * Purpose: <WHY DID YOU MAKE THIS?>
+ */
+
+// System libraries
+#include <iostream>
+
+using namespace std;
+
+// user libraries (if any)
+
+// global constants
+// used for science, math, conversion, dimensions. Things that never change
+
+// function prototypes
+
+//execution starts here at the main function
+int main( int argc, char** argv ) {    
+    // declare variables
+    
+    // initialize variables
+    
+    // process the inputs -> outputs (IO)
+    
+    // display inputs/outputs
+    
+    // clean up memory and files
+    
+    // exit the Program
+    return 0;
+}
